2da0614f62ff45c316247631b4891ac20019825d
Maciej Milarski <maciekmilarski123@gmail.com>
2023-03-28 19:13:18 +0200

    added show table and sold quantity

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-items = []
+items = [{"name": "cebula", "quantity": 99, "unit_name": "kg", "unit_price": 4.5},
+         {"name": "papryka", "quantity": 44, "unit_name": "kg", "unit_price": 4}]
 def add_item():
     name = input("Item name: ")
     quantity = input("Item quantity: ")
     unit_name = input("Item unit of measure Eg. l, kg, pcs: ")
     unit_price = input("Item price in PLN: ")
-    items.append({"name": name, "quantity": quantity, "unit_name": unit_name, "unit_price": unit_price})
+    items.append({"name": name, "quantity": int(quantity), "unit_name": unit_name, "unit_price": float(unit_price)})
+
     print("Successfully added to warehouse. Current status:")
     show_warehouse()
+
+def sell_item(name, quantity_sold):
+    for item in items:
+        if item["name"] == name:
+            item["quantity"] -= quantity_sold
+            print(f"Successfully sold {quantity_sold} {item['unit_name']}")
+
+
 
 def exit_warehouse():
     print("Exiting... Bye!")
     exit()
 
 def show_warehouse():
-    print("Name\tQuantity\tUnit\tUnit Price (PLN)")
+    print("""Name\t\t\tQuantity\tUnit\tUnit Price (PLN)
+    ---------------------------------------""")
     for x in range(len(items)):
-        print(f"{items[x]['name']}\t{items[x]['quantity']}\t{items[x]['unit_name']}\t{items[x]['unit_price']}")
+        print(f"{items[x]['name']:<16}{items[x]['quantity']:<12}{items[x]['unit_name']:<8}{items[x]['unit_price']}")
+
+
 
 if __name__ == '__main__':
     while True:
         inp = input("What would you like to do? ")
         if inp == "add":
             add_item()
+        if inp == "sell":
+            name = input("Item name: ")
+            quantity_sold = input("Quantity to sell: ")
+            sell_item(name, int(quantity_sold))
         if inp == "show":
             show_warehouse()
         if inp == "exit":
